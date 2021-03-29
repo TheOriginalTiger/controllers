b0072f31b2c789afac376d0bc4335f8aee5220cb
TheOriginalTiger <vvp.vovarulit@gmail.com>
2021-03-29 20:37:25 +0700

    started working on that button from other scheme stuff

--- main.c
+++ main.c
@@ -3,29 +3,66 @@
 
 
 
-volatile uint32_t timerCounter = 0;
+volatile uint8_t flag = 0 ;
+
+volatile uint8_t pressed1 = 0;
+volatile uint8_t pressed2 = 0;
+volatile uint8_t pressed3 = 0;
+volatile uint8_t pressed4 = 0;
 
 void SysTick_Handler(void)
 {
-	timerCounter++;
+
+	pressed1 = pressed2 = pressed3 = pressed4 = 0;
+
+	if(flag)
+	{
+		flag = 0;
+		if(GPIOA->IDR & GPIO_IDR_4)
+			pressed1 = 1;
+		if (GPIOA->IDR & GPIO_IDR_5)
+			pressed2 = 1 ;
+
+		GPIOC->ODR |= GPIO_ODR_12;
+		GPIOA->ODR &= ~GPIO_ODR_15;
+
+		//GPIOC->BSRR =  GPIO_BSRR_BR_12 ;
+		//GPIOA->BSRR =  GPIO_BSRR_BS_15 ;
+	}
+	else
+	{
+		flag =1;
+		if(GPIOA->IDR & GPIO_IDR_4)
+			pressed3 = 1;
+		if (GPIOA->IDR & GPIO_IDR_5)
+			pressed4 = 1 ;
+		//GPIOC->BSRR = GPIO_BSRR_BS_12 ;
+		//GPIOA->BSRR = GPIO_BSRR_BR_15 ;
+		GPIOC->ODR &= ~GPIO_ODR_12;
+		GPIOA->ODR |= GPIO_ODR_15;
+
+	}
 }
 
 
 static void Init3(void)
 {
 	RCC->AHBENR |= (RCC_AHBENR_GPIOCEN | RCC_AHBENR_GPIOAEN);
-	GPIOC->MODER |= (GPIO_MODER_MODER6_0 | GPIO_MODER_MODER7_0 | GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0);
+	GPIOC->MODER |= (GPIO_MODER_MODER6_0 | GPIO_MODER_MODER7_0 | GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0 | GPIO_MODER_MODER12_0);
+	GPIOC->PUPDR |=  (GPIO_PUPDR_PUPDR6_1 | (GPIO_PUPDR_PUPDR7_1) | (GPIO_PUPDR_PUPDR8_1) | (GPIO_PUPDR_PUPDR9_1));
+	GPIOA->PUPDR |= (GPIO_PUPDR_PUPDR4_1 | GPIO_PUPDR_PUPDR5_1);
+	GPIOA->MODER |= GPIO_MODER_MODER15_0;
 
 	SystemCoreClockUpdate();
-	SysTick->LOAD = SystemCoreClock  - 1;
-	SysTick->VAL =  SystemCoreClock  - 1;
+	SysTick->LOAD = SystemCoreClock/1000  - 1;
+	SysTick->VAL =  SystemCoreClock/1000 - 1;
 	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_TICKINT_Msk;
 }
 
 
 /*
  * SysTick_CTRL_TICKINT_Msk enable interrupt
- */
+
 static void task3(void)
 {
 
@@ -64,9 +101,22 @@ static void task3(void)
 		GPIOC->ODR &= ~GPIO_ODR_7;
 	}
 }
-
+*/
 int main(void)
 {
 	Init3();
-	task3();
+	while(1)
+	{
+		uint32_t mask = 0;
+		if (pressed1)
+			mask |= GPIO_ODR_6;
+		if (pressed2)
+			mask |= GPIO_ODR_7;;
+		if (pressed3)
+			mask |= GPIO_ODR_8;;
+		if (pressed4)
+			mask |= GPIO_ODR_9;;
+		GPIOC->ODR |= mask;
+		GPIOC->ODR &= ~(GPIO_ODR_6 | GPIO_ODR_7 | GPIO_ODR_8 | GPIO_ODR_9);
+	}
 }
